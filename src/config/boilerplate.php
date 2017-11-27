78113f166d3b2f18555dfb8198d47d3a666aa1e1
Lucho <lucho2d7@gmail.com>
2017-11-27 17:01:19 -0300

    API boilerplate config modification

--- src/config/boilerplate.php
+++ src/config/boilerplate.php
@@ -5,7 +5,15 @@ return [
     'sign_up' => [
         'release_token' => env('SIGN_UP_RELEASE_TOKEN'),
         'validation_rules' => [
-            'name' => 'required',
+            'name' => 'required|min:2|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|max:255|confirmed'
+        ]
+    ],
+
+    'sign_up_verification' => [
+        'release_token' => env('SIGN_UP_VERIFICATION_RELEASE_TOKEN'),
+        'validation_rules' => [
             'email' => 'required|email',
             'password' => 'required'
         ]
@@ -29,7 +37,7 @@ return [
         'validation_rules' => [
             'token' => 'required',
             'email' => 'required|email',
-            'password' => 'required|confirmed'
+            'password' => 'required|min:8|max:255|confirmed'
         ]
     ]
 
