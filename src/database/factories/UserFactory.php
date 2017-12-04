5f68fb4a9db49a519dbedb92f5d3fdcc5ca1aadc
Lucho <lucho2d7@gmail.com>
2017-12-04 17:53:16 -0300

    changed User factory default password to comply with validation rules

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = 'secret',
+        'password' => $password ?: $password = 'secret01',
         'remember_token' => str_random(10),
         'role' => User::ROLE_USER,
         'status' => User::STATUS_ENABLED,
